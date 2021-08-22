729d0025a016f6198b17b0cd97e249da6712940c
Vanessa Nascimento <vanessa.nascimento@picpay.com>
2021-08-22 17:51:47 -0300

    feat: add funcionalidade de adicionar, checar e remover uma tarefa

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -13,17 +13,35 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
-
+  const [randomId, setRandomId] = useState(0);
+  const [errorTask, setErrorTask] = useState(false);
+  
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    setRandomId(randomId + 1);
+
+    newTaskTitle ? (
+      setTasks(
+        [...tasks, {
+          id: randomId,
+          title: newTaskTitle,
+          isComplete: false
+        }]
+      ), 
+      setErrorTask(false)
+    ) : setErrorTask(true);
+
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => task.id === id ? {...task, isComplete: true} : task))
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
   return (
@@ -42,6 +60,11 @@ export function TaskList() {
             <FiCheckSquare size={16} color="#fff"/>
           </button>
         </div>
+        {errorTask ? (
+          <div className="input-error">
+            <p>Não há tarefa para incluir!</p>
+          </div> 
+        ) : ''}
       </header>
 
       <main>
@@ -66,7 +89,6 @@ export function TaskList() {
               </button>
             </li>
           ))}
-          
         </ul>
       </main>
     </section>
